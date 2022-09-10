e0cab32c62837622a4813fa437147e8969a87f65
bladewind21 <bladewind21@gmail.com>
2022-09-09 22:54:28 -0500

    adding supervisor controller tests

--- src/test/java/io/lightfeather/controller/SupervisorControllerTest.java
+++ src/test/java/io/lightfeather/controller/SupervisorControllerTest.java
@@ -1,6 +1,5 @@
 package io.lightfeather.controller;
 
-import io.lightfeather.entity.Supervisor;
 import io.lightfeather.service.SupervisorService;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,13 +13,18 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import javax.xml.bind.ValidationException;
 import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.hamcrest.Matchers.containsString;
+
 
 @ExtendWith(MockitoExtension.class)
 public class SupervisorControllerTest {
@@ -55,6 +59,116 @@ public class SupervisorControllerTest {
                 get("/api/supervisors")
                         .contentType(MediaType.APPLICATION_JSON)
         ).andExpect(status().isOk()).andExpect(content().string(containsString("CA - Doe, Jane")));
+
+    }
+
+    @SneakyThrows
+    @Test
+    void postSupervisor(){
+
+        mockMvc.perform(
+                post("/api/supervisors")
+                        .contentType(MediaType.APPLICATION_JSON).content("{firstName: \"John\", " +
+                                "lastName: \"Smith\", " +
+                                "email: \"something@gmail.com\", " +
+                                "phoneNumber: \"1-234-5678\", " +
+                                "supervisor: \"CA - Doe, Jane\"}")).andExpect(status().isOk());
+               }
+
+    @SneakyThrows
+    @Test
+    void postSupervisorShort(){
+
+        mockMvc.perform(
+                post("/api/supervisors")
+                        .contentType(MediaType.APPLICATION_JSON).content("{firstName: \"John\", " +
+                                "lastName: \"Smith\", " +
+                                "supervisor: \"CA - Doe, Jane\"}")).andExpect(status().isOk());
+    }
+
+    @SneakyThrows
+    @Test
+    void postSupervisorWithPhoneValidationErrors(){
+
+        Exception exception = assertThrows(ValidationException.class, () ->{
+        mockMvc.perform(
+                post("/api/supervisors")
+                        .contentType(MediaType.APPLICATION_JSON).content("{firstName: \"John\", " +
+                                "lastName: \"Smith\", " +
+                                "email: \"something@gmail.com\", " +
+                                "phoneNumber: \"1d234d5678\", " +
+                                "supervisor: \"CA - Doe, Jane\"}")).andExpect(status().isOk());
+        });
+
+        String expectedMessage = "phoneNumber is not valid";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+
+    }
+
+
+    @SneakyThrows
+    @Test
+    void postSupervisorWithEmailValidationErrors(){
+
+        Exception exception = assertThrows(ValidationException.class, () ->{
+            mockMvc.perform(
+                    post("/api/supervisors")
+                            .contentType(MediaType.APPLICATION_JSON).content("{firstName: \"John\", " +
+                                    "lastName: \"Smith\", " +
+                                    "email: \"somethingcom\", " +
+                                    "phoneNumber: \"1-234-5678\", " +
+                                    "supervisor: \"CA - Doe, Jane\"}")).andExpect(status().isOk());
+        });
+
+        String expectedMessage = "email is not valid";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+
+    }
+
+    @SneakyThrows
+    @Test
+    void postSupervisorWithFirstNameValidationErrors(){
+
+        Exception exception = assertThrows(ValidationException.class, () ->{
+            mockMvc.perform(
+                    post("/api/supervisors")
+                            .contentType(MediaType.APPLICATION_JSON).content("{firstName: \"Jo3hn\", " +
+                                    "lastName: \"Smith\", " +
+                                    "email: \"something@gmail.com\\" +
+                                    "phoneNumber: \"1-234-5678\", " +
+                                    "supervisor: \"CA - Doe, Jane\"}")).andExpect(status().isOk());
+        });
+
+        String expectedMessage = "firstName is not valid";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+
+    }
+
+    @SneakyThrows
+    @Test
+    void postSupervisorWithLasttNameValidationErrors(){
+
+        Exception exception = assertThrows(ValidationException.class, () ->{
+            mockMvc.perform(
+                    post("/api/supervisors")
+                            .contentType(MediaType.APPLICATION_JSON).content("{firstName: \"John\", " +
+                                    "lastName: \"Smi3th\", " +
+                                    "email: \"something@gmail.com\", " +
+                                    "phoneNumber: \"1-234-5678\", " +
+                                    "supervisor: \"CA - Doe, Jane\"}")).andExpect(status().isOk());
+        });
+
+        String expectedMessage = "lastName is not valid";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+
     }
 
 
